93a0dae0f61fef5d5bf6b86e7d9a944cf3f9e674
TaeHyeon Kim <kth990303@naver.com>
2021-02-17 19:43:47 +0900

    필수요구사항 준수
    210215 이슈 중 필수요구사항을 준수한 코드. src코드만 올렸습니다 (.yml 코드 제외)

---
+++ src/test/java/MemberServiceTest.java
@@ -0,0 +1,54 @@
+import algopa.study.domain.Member;
+import algopa.study.repository.MemoryMemberRepository;
+import algopa.study.service.MemberService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class MemberServiceTest {
+    MemberService service;
+    MemoryMemberRepository repository;
+
+    @BeforeEach
+    public void beforeEach(){
+        repository=new MemoryMemberRepository();
+        service=new MemberService(repository);
+    }
+
+    @AfterEach
+    public void afterEach(){
+        repository.clearStore();
+    }
+
+    @Test
+    public void join(){
+        //given
+        Member member=new Member();
+        member.setName("kth990303");
+        member.setTier("Platinum II");
+        //when
+        Long saveId=service.join(member);
+        //then
+        Member result = repository.findByName("kth990303").get();
+        assertEquals(member.getName(), result.getName());
+        assertEquals(member.getTier(), result.getTier());
+    }
+
+    @Test
+    public void duplicateMember() throws Exception{
+        Member m1=new Member();
+        Member m2=new Member();
+        m1.setName("kth990303");
+        m1.setTier("Platinum II");
+        m2.setName("kth990303");
+        m2.setTier("Platinum III");
+        service.join(m1);
+
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> service.join(m2));
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+    }
+}
